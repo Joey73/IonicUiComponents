6e566eb886a034664e89c5f81119aa3cb9d00638
Joerg Schmitz <schmitz873@gmail.com>
2017-08-15 00:04:31 +0200

    Added  a counter

--- src/pages/gestures1/gestures1.ts
+++ src/pages/gestures1/gestures1.ts
@@ -20,13 +20,34 @@ export class Gestures1Page {
 
   onElementClicked(){
     console.log('I was clicked or touched');
+    this.clicked++;
   }
   
   onElementTapped(){
     console.log('I was tapped');
+    this.tapped++;
   }
 
   onElementPressed() {
     console.log('I was pressed');
+    this.pressed++;
+  }
+
+  onReset(type: string){
+    switch(type) {
+      case 'clicks':
+        this.clicked = 0;;
+        break;
+      case 'taps':
+        this.tapped = 0;
+        break;
+      case 'presses':
+        this.pressed = 0;
+        break;
+      default:
+        this.clicked = 0;
+        this.tapped = 0;
+        this.pressed = 0;
+    }
   }
 }
