bbe53018dba8e337b029fa29ddf0e78413210339
Joerg Schmitz <schmitz873@gmail.com>
2017-08-14 23:39:33 +0200

    Added custom component

--- src/components/touch-event.component.ts
+++ src/components/touch-event.component.ts
@@ -2,20 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-touch-event',
-    template: `
-        <div class="gestures"
-            (click)="onElementClicked()">
-            Click me
-        </div>
-        <div class="gestures"
-            (tap)="onElementTapped()">
-            Tap me
-        </div>
-        <div class="gestures"
-            (press)="onElementPressed()">
-            Long Press me
-        </div>
-    `
+    templateUrl: 'touch-event.component.html',
 })
 export class TouchEventComponent {
   onElementClicked(){
